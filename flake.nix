af0716fe6dfe6264b7c90b8219ee70f9b7de7487
jswartzendruber <jsw695538@gmail.com>
2024-04-17 21:17:12 -0400

    initial config

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Config flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations.box = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.jason = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
