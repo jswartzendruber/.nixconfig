b82723382855c00ee85d45c7dc17eb74dab7c6c9
jswartzendruber <jsw695538@gmail.com>
2024-04-24 18:44:51 -0400

    fix audio

--- modules/system.nix
+++ modules/system.nix
@@ -35,8 +35,11 @@
     vim
   ];
 
-  sound.enable = true;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
 }
