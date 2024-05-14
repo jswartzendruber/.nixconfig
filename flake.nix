0e01d2d2a95ced23ecc26245f02a7e334ca2def6
jswartzendruber <jsw695538@gmail.com>
2024-05-14 18:09:06 -0400

    use nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
