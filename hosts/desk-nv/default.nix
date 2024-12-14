a083b3ee71e5fdf38e544db194809e90b4b2a478
jswartzendruber <jsw695538@gmail.com>
2024-12-14 17:19:39 -0500

    try open nvidia driver

--- hosts/desk-nv/default.nix
+++ hosts/desk-nv/default.nix
@@ -37,7 +37,7 @@
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = false; 
+    open = true; 
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
